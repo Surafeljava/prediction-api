a668a7c256f110eb471af7bccbf55a1ea625f7df
Surafeljava <surafelk27@gmail.com>
2022-06-16 18:14:42 +0300

    fix: packages

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, request, jsonify
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.models import load_model
+# from tensorflow.keras.preprocessing.sequence import pad_sequences
+# from tensorflow.keras.models import load_model
 import pickle
 
 app = Flask(__name__)
@@ -9,23 +9,24 @@ app = Flask(__name__)
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 os.environ["MKLDNN_VERBOSE"] = "1"
 
-model = load_model('./asa_model.h5')
+# model = load_model('./asa_model.h5')
 
 
 def predict_from_word(word):
 
-    with open('asa_tokenizer.pickle', 'rb') as handle:
-        tokenizer = pickle.load(handle)
+    # with open('asa_tokenizer.pickle', 'rb') as handle:
+    #     tokenizer = pickle.load(handle)
 
-    # Create a token for the work
-    token = tokenizer.texts_to_sequences([word])
-    tw = pad_sequences(token, maxlen=256)
+    # # Create a token for the work
+    # token = tokenizer.texts_to_sequences([word])
+    # tw = pad_sequences(token, maxlen=256)
 
-    # Predict with the model
-    prediction = model.predict(tw).item()
-    print('\n', word, '\n', prediction)
+    # # Predict with the model
+    # prediction = model.predict(tw).item()
+    # print('\n', word, '\n', prediction)
 
-    return prediction
+    # return prediction
+    return "Prediction Result here"
 
 
 @app.route('/', methods=['GET'])
